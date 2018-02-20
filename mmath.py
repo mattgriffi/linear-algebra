2c059c319f269af8ca3ea48fd879fe306eb7fad7
mseffner <mseffner@gmail.com>
2018-02-20 12:43:52 -0700

    Implemented trace

--- mmath.py
+++ mmath.py
@@ -13,3 +13,9 @@ def transpose(A):
     """Calculates the transpose of Matrix A.
     """
     return Matrix(A.rows)
+
+
+def trace(A):
+    """Calculates the sum of the leading diagonal of Matrix A.
+    """
+    return sum(A[i][i] for i in range(min(A.dimension())))
