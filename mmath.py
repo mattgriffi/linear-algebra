15919b07a56177e77792fbb5d5c4e00aa0aebb09
mseffner <mseffner@gmail.com>
2018-02-20 11:41:56 -0700

    Implemented transpose

--- mmath.py
+++ mmath.py
@@ -7,3 +7,9 @@ import math
 
 from matrix import Matrix, check_dimensions
 from vector import Vector
+
+
+def transpose(A):
+    """Calculates the transpose of Matrix A.
+    """
+    return Matrix(A.rows)
