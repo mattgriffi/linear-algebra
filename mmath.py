524f93c41fa062a8ed66814e6e52cfe06241a031
mseffner <mseffner@gmail.com>
2018-02-20 08:28:19 -0700

    Added imports to mmath

--- mmath.py
+++ mmath.py
@@ -1,3 +1,9 @@
 """This file defines some useful functions for performing calculations
 with Matrices.
 """
+
+import itertools
+import math
+
+from matrix import Matrix, check_dimensions
+from vector import Vector
