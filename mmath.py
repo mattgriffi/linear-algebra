5a3f01f7bdc4ba54179d8661a46780bfcd559059
mseffner <mseffner@gmail.com>
2018-02-20 23:49:37 -0700

    Fixed bugs and added input validation to mmath

--- mmath.py
+++ mmath.py
@@ -2,12 +2,8 @@
 with Matrices.
 """
 
-import itertools
-import math
-
 import vmath
-from matrix import Matrix, check_dimensions
-from vector import Vector
+from matrix import Matrix, DimensionError
 
 
 def transpose(A):
@@ -19,14 +15,23 @@ def transpose(A):
 def trace(A):
     """Calculates the sum of the leading diagonal of Matrix A.
     """
-    return sum(A[i][i] for i in range(min(A.dimension())))
+    return sum(A[i][i] for i in range(min(A.dim)))
 
 
 def factorize(A):
     """Performs QR factorization on invertible Matrix A. Returns (Q, R).
     """
+    if A.dim.rows != A.dim.columns:
+        raise DimensionError("Matrix A must be square.")
+
     # Use Gram-Schidt to calculate orthogonal Matrix Q from columns of A
     Q = Matrix(vmath.gs(A.columns), columns=True)
+
+    # If A is not invertible, vmath.gs will not return a square matrix
+    if A.dim != Q.dim:
+        raise DimensionError("Matrix A must be invertible.")
+
     # Upper triangular Matrix R = (Q^T)A
     R = transpose(Q) * A
+
     return Q, R
