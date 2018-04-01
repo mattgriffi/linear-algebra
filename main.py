c06bdb3a09d1f4769efced635ba238083aed54c9
mseffner <mseffner@gmail.com>
2018-04-01 11:04:20 -0700

    added printf helper function

--- main.py
+++ main.py
@@ -13,32 +13,24 @@ from vector import Vector
 
 def main():
     T = Matrix(
-        (3, 1, 1),
-        (1, -3, -1)
+        (1/3, 2/3),
+        (2/3, 1/3)
     )
+    x = Vector(1, 0)
 
-    B = [
-        Vector(1, 1, 1),
-        Vector(-1, 0, 1),
-        Vector(0, 0, 1)
-    ]
+    for i in range(1, 5):
+        printm(mmath.power(T, i) * x)
     
-    C = [
-        Vector(1, 2),
-        Vector(-1, 1)
-    ]
-
-    A = mmath.get_transformation_matrix(T, B, C)
-    u = Vector(1, 0, 2)
-
-    printm(A * mmath.change_basis(u, B))
-
-    for e in vmath.get_standard_unit_vectors(5):
-        printm(e)
+    printm(mmath.power(T, 100) * x)
 
 
 def printm(A):
     print(A)
+    print()
+
+
+def printf(A):
+    print(A.str_fractions())
     print()
 
 
